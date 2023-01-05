47253a07a8bca97a6a88acc0d265d62c68c1479e
archi756 <69085594+archi756@users.noreply.github.com>
2023-01-05 07:38:42 -0800

    hhhh

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ var bgMusicControls = true; // Show UI music control
 // ===================== start =======================
 // animation start after 1000 miliseconds
 setTimeout(init, 1000);
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
 
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
 var odrag = document.getElementById('drag-container');
 var ospin = document.getElementById('spin-container');
 var aImg = ospin.getElementsByTagName('img');
